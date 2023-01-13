add3e6d8e8c279607cd554f0875835fdf023e94d
j45bongsik <dnrlekzz@naver.com>
2023-01-13 14:06:42 +0900

    404 clone

---
+++ src/router/NonRoute copy.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+// import Kakao from '../components/contents/Kakao'
+import Kakao from '../components/contents/_Kakao'
+// import { ContStyle } from '../styles/Contents';
+import { ContStyle } from '../styles/Contents';
+import Header from '../components/header/Header'
+
+
+const NonRoute = (props) => {
+    
+
+    return (
+        <>
+            <Header></Header>
+            <ContStyle.contArea>
+                <Kakao />
+            </ContStyle.contArea>
+        </>
+    ) 
+
+}
+
+export default NonRoute;
